ac93ab04f8c6941a8e5c563d69b1356a765c5959
khegazy <KareemHegz@gmail.com>
2024-07-19 21:01:12 -0700

    initial commit

---
+++ scOT/transformerTS.py
@@ -0,0 +1,42 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from .layers.transformer import Decoder, DecoderLayer, Encoder, EncoderLayer, ConvLayer
+from .layers.self_attention import FullAttention, AttentionLayer
+from .layers.embed_transformer import DataEmbedding
+
+class TransformerTS(nn.Module):
+    def __init__(self, configs):
+        super().__init__()
+        self.pred_len = configs.pred_len
+        self.output_attention = configs.output_attention
+
+        if configs.channel_independence:
+            self.enc_in = 1
+            self.dec_in = 1
+            self.c_out = 1
+        else:
+            self.enc_in = configs.enc_in
+            self.dec_in = configs.dec_in
+            self.c_out = configs.c_out
+
+        # Embedding
+        self.enc_embedding = DataEmbedding(self.enc_in, configs.d_model, configs.embed, configs.freq,
+                                           configs.dropout)
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(
+                        FullAttention(False, configs.factor, attention_dropout=configs.dropout,
+                                      output_attention=configs.output_attention), configs.d_model, configs.n_heads),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation
+                ) for l in range(configs.e_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(configs.d_model)
+        )
+
+
